2f687bdae220582e99647f8c3c819f3b5020ef85
Michael Scheuringer <m.scheuringer@students.htl-leonding.ac.at>
2025-03-12 12:58:03 +0100

    root

---
+++ project/JS/main.js
@@ -0,0 +1,13 @@
+/*********************/
+/*Michael Scheuringer*/
+/*------Main-JS------*/
+/*********************/
+
+//Hover
+function loadPlayButton() {
+    document.getElementById('buttonPlayImg').src = "./medien/items/revolver (1).gif";
+}
+
+function unLoadPlayButton() {
+    document.getElementById('buttonPlayImg').src = "./medien/items/revolver (2).gif";
+}
