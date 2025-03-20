4875af50518ea0e5dfc122cb8e4435988e1a6cb3
Michael Scheuringer <m.scheuringer@students.htl-leonding.ac.at>
2025-03-20 13:35:16 +0100

    backgrounds+font

--- project/JS/main.js
+++ project/JS/main.js
@@ -3,11 +3,49 @@
 /*------Main-JS------*/
 /*********************/
 
-//Hover
+// **** Hover ****
 function loadPlayButton() {
     document.getElementById('buttonPlayImg').src = "./medien/items/revolver (1).gif";
 }
 
 function unLoadPlayButton() {
     document.getElementById('buttonPlayImg').src = "./medien/items/revolver (2).gif";
+}
+// **** Side switch functions ****
+function switchToSettings(){
+    killSides();
+    document.getElementById('settings_side').style.display = 'block'
+}
+function switchToStart(){
+    killSides();
+    document.getElementById('start_side').style.display = 'block'
+}
+function switchToSave(){
+    killSides();
+    document.getElementById('savedGames_side').style.display = 'block'
+}
+function switchToPlay(){
+    killSides();
+    document.getElementById('world_side').style.display = 'block'
+}
+function switchToShop(){
+    killSides();
+    document.getElementById('shop_side').style.display = 'block'
+}
+function switchToSaloon(){
+    killSides();
+    document.getElementById('saloon_side').style.display = 'block'
+}
+function openOptions(){
+    document.getElementById('world_side_options').style.display = 'block'
+    
+}
+function killSides(){
+    document.getElementById('start_side').style.display = 'none';
+    document.getElementById('world_side_options').style.display = 'none';
+    document.getElementById('world_side').style.display = 'none';
+    document.getElementById('shop_side').style.display = 'none';
+    document.getElementById('saloon_side').style.display = 'none';
+    document.getElementById('savedGames_side').style.display = 'none';
+    document.getElementById('settings_side').style.display = 'none';
 }
